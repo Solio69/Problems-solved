10c9cb70bdda4882359517faae6a51a28258b91f
Anastasiya Kazanina <61517307+Solio69@users.noreply.github.com>
2020-12-21 14:02:26 +0300

    Friend or Foe

---
+++ Friend or Foe.js
@@ -0,0 +1,20 @@
+// Создайте программу, которая фильтрует список строк и возвращает список только с именами ваших друзей.
+
+// Если в имени ровно 4 буквы, можете быть уверены, что это должен быть ваш друг!В противном случае можете быть уверены, что он не...
+
+//Пример: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"].
+
+// т.е.
+
+// friend ["Ryan", "Kieran", "Mark"]
+
+// const friend = (friends) => {
+//     return friends.filter(item => item.length === 4);
+// };
+
+const friend = friends => friends.filter(friend => friend.length == 4);
+
+console.log(friend(["Ryan", "Kieran", "Mark"]));
+
+
+// friend(["Ryan", "Kieran", "Mark"]); // ["Ryan", "Mark"]
